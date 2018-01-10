6914b2b58afe936a1650ddbeddeef2131e8655fa
Adam Zygadlewicz <adam@zygadlewicz.com>
2018-01-10 15:19:07 +0100

    Updates

--- src/meetup-redux/sagas.js
+++
@@ -1,9 +0,0 @@
-//import sagaExamples from './sagaExamples';
-
-import sagaAuth from './sagaAuth';
-
-
-export default [
-	sagaAuth,
-   // sagaExamples
-];
--- src/meetup-redux/types.js
+++
@@ -1,5 +0,0 @@
-
-
-
-export const CHANGE_THEME = 'CHANGE_THEME';
-export const SET_USER = 'SET_USER';
